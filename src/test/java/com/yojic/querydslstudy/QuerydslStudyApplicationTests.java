2f3a16a7315566d26bc0a371b5962d638c6d088f
YOJIC-J <dywlr74@hmcnetworks.co.kr>
2023-12-27 10:56:19 +0900

    Initial commit

---
+++ src/test/java/com/yojic/querydslstudy/QuerydslStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yojic.querydslstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
