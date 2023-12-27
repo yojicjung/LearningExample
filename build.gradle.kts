9185c5a0edd581e003cd02a20b71fd0269e53f7d
YOJIC-J <dywlr74@hmcnetworks.co.kr>
2023-12-27 10:56:49 +0900

    최초 커밋

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+val jar: Jar by tasks
+val bootJar: BootJar by tasks
+
+bootJar.enabled = false
+jar.enabled = true
+
 plugins {
-    java
-    id("org.springframework.boot") version "3.2.1"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.1.5"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.spring") version "1.9.20"
+    kotlin("plugin.jpa") version "1.9.20"
+    kotlin("kapt") version "1.9.20"
 }
 
 group = "com.yojic"
@@ -10,14 +21,46 @@ version = "0.0.1-SNAPSHOT"
 java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // jpa, querydsl
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.5")
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+
+    kapt("org.springframework.boot:spring-boot-configuration-processor:2.7.2")
+    // mysql-connector
+    implementation("com.mysql:mysql-connector-j:8.0.31")
+
+    // mapstructs
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.5")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc:3.0.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 tasks.withType<Test> {
