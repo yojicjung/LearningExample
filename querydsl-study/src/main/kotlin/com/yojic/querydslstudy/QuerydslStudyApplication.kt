70179c7a8739c6f481f26c714b040276941438ef
yojic <dywlr74@naver.com>
2023-12-27 21:02:36 +0900

    QuerydslStudy

---
+++ querydsl-study/src/main/kotlin/com/yojic/querydslstudy/QuerydslStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.yojic.querydslstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuerydslStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<QuerydslStudyApplication>(*args)
+}
